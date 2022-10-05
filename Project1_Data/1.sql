7f4150b61e033f5564b2b12ded95986fe7e6a599
Liliia Aliakberova <108422927+Liliia-Aliakberova@users.noreply.github.com>
2022-10-05 21:24:24 +0200

    Update 1.sql
    #Initial query №1

--- Project1_Data/1.sql
+++ Project1_Data/1.sql
@@ -1,8 +1,9 @@
-select 
-a.customer_id, a.last_name
-from public.customer a
-inner join public.rental b on b.customer_id = a.customer_id
-inner join public.inventory c ON c.inventory_id = b.inventory_id
-inner join public.film d on d.film_id = c.film_id
+#Initial query №1
+
+select customer.customer_id, customer.last_name
+from public.customer
+inner join public.rental on rental.customer_id = customer.customer_id
+inner join public.inventory ON inventory.inventory_id = rental.inventory_id
+inner join public.film on film.film_id = inventory.film_id
 group by 1
 having max(d.length/60) < 3
