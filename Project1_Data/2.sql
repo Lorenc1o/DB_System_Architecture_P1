771c88e8b673ca7c2cd26f21ff83d720dc30e0e7
Liliia Aliakberova <108422927+Liliia-Aliakberova@users.noreply.github.com>
2022-10-26 23:07:50 +0200

    Optimization

--- Project1_Data/2.sql
+++ Project1_Data/2.sql
@@ -6,3 +6,16 @@ SELECT country.country
 	and address.address_id=customer.address_id
 		GROUP BY country.country_id, city.city_id
 		having count (customer.customer_id)> 1 
+		
+-- Optimized fastest query, no indexes need to be used (AVG (Addition of Planning time + Execution time per execution) = 2.9055 ms)
+WITH cte 
+AS (
+    SELECT city.country_id
+    FROM customer
+    INNER JOIN address ON customer.address_id = address.address_id
+    INNER JOIN city ON city.city_id = address.city_id
+    GROUP BY city.city_id
+    HAVING COUNT (customer.customer_id) > 1)
+SELECT country.country
+FROM cte
+INNER JOIN country ON country.country_id = cte.country_id
