73150330fe081ea9c3ef88267674b3ad3bf3aa0c
Mariana M Llano <marianiwis9@hotmail.com>
2022-10-30 14:34:16 +0100

    Optimized Q1 changing query structure
    Considering previous indexes. Fastest result using just subqueries, except statement and distinct in shortMovies subquery

--- Project1_Data/1.sql
+++ Project1_Data/1.sql
@@ -25,3 +25,57 @@ INNER JOIN public.inventory ON inventory.inventory_id = rental.inventory_id
 INNER JOIN public.film ON film.film_id = inventory.film_id
 GROUP BY customer.customer_id
 HAVING MAX(film.length) < 180;
+
+
+-- (in Mariana's PC) Optimized changing query structure, considering indexes mentioned above 
+
+-- Algorithm changes made: Use subqueries to filter before joining.
+-- Syntax versions tested:
+-- -> Using subqueries vs With
+-- -> Using inner joins vs merging in where statement
+-- -> Using distinct vs not using distinct in subquery shortMovies
+
+
+-- Fastest ( 14.5629 ms avg)
+-- just subqueries, except, with distinct in subquery
+Select customer.customer_id, customer.last_name
+from 
+	(Select rental.customer_id
+	from public.rental 
+	EXCEPT
+		SELECT distinct rental.customer_id
+		FROM (SELECT film.film_id
+		from public.film
+		where film.length >= 180) as longMovies 
+		inner join public.inventory on longMovies.film_id = inventory.film_id
+		inner join public.rental on rental.inventory_id = inventory.inventory_id) as shortMovies
+inner join public.customer on shortMovies.customer_id = customer.customer_id
+
+
+
+
+
+
+-- Second Fastest ( 14.5629 ms avg)
+-- merge customer in where, with, except, no distinct in subquery
+-- AVG time: 15.7921
+
+
+With longMovies as	(
+	SELECT film.film_id
+	from public.film
+	where film.length >= 180)
+	
+Select customer.customer_id, customer.last_name
+from 
+	(
+	Select rental.customer_id
+	from public.rental 
+	EXCEPT
+	SELECT rental.customer_id
+	FROM longMovies 
+	inner join public.inventory on longMovies.film_id = inventory.film_id
+	inner join public.rental on rental.inventory_id = inventory.inventory_id
+) as shortMovies
+,public.customer 
+where shortMovies.customer_id = customer.customer_id
