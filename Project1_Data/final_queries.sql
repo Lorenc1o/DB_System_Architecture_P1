18d5e5cd2bb0a0ba125749f650bc064abba753c0
Mariana M Llano <marianiwis9@hotmail.com>
2022-11-02 12:51:48 +0100

    Q2 updated

--- Project1_Data/final_queries.sql
+++ Project1_Data/final_queries.sql
@@ -18,17 +18,16 @@ HAVING MAX(film.length) < 180;
 
 --Query №2
 
-WITH cte 
-AS (
-    SELECT city.country_id
-    FROM customer
-    INNER JOIN address ON customer.address_id = address.address_id
-    INNER JOIN city ON city.city_id = address.city_id
-    GROUP BY city.city_id
-    HAVING COUNT (customer.customer_id) > 1)
-SELECT country.country
-FROM cte
-INNER JOIN country ON country.country_id = cte.country_id;
+select country.country
+from 
+	(
+	Select address.city_id
+	from public.customer inner join public.address on address.address_id = customer.address_id
+	group by address.city_id
+	having count(customer.customer_id) > 1
+	) as filtered
+inner join public.city on city.city_id = filtered.city_id
+inner join public.country on country.country_id = city.country_id
 
 
 --Query №3
